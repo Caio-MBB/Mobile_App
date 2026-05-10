e5c47d0d3d198517f11024229c7d00c87a255ed7
Caio Borges <caioborges@MacBookPro.station>
2026-05-10 16:26:57 +0100

    implementing movie service with all tmdb api configurations

--- src/app/services/movie.ts
+++ src/app/services/movie.ts
@@ -1,8 +1,43 @@
 import { Injectable } from '@angular/core';
+import { CapacitorHttp, HttpResponse } from '@capacitor/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Movie {
+
+  private apiKey = null; // change afterwards
+  private baseUrl = 'https://api.themoviedb.org/3';
+
+  async getTrendingMovies(){
+    const response: HttpResponse = await CapacitorHttp.get({
+      url: `${this.baseUrl}/trending/movie/day?api_key=${this.apiKey}`
+    });
+    return response.data;
+  }
+  async searchMovies(query: string){
+    const response: HttpResponse = await CapacitorHttp.get({
+      url: `${this.baseUrl}/search/movie?query=${query}&api_key=${this.apiKey}`
+    })
+    return response.data;
+  } 
+  async getMovieCredits(id: number){
+    const response: HttpResponse = await CapacitorHttp.get({
+      url: `${this.baseUrl}/movie/${id}/credits?api_key=${this.apiKey}`
+    })
+    return response.data;
+  }
+  async getPersonDetails(id: number){
+    const response: HttpResponse = await CapacitorHttp.get({
+      url: `${this.baseUrl}/person/${id}?api_key=${this.apiKey}`
+    })
+    return response.data;
+  }
   
+  async getPersonCredits(id: number){
+    const response: HttpResponse = await CapacitorHttp.get({
+      url: `${this.baseUrl}/person/${id}/movie_credits?api_key=${this.apiKey}`
+    })
+    return response.data;
+  }
 }
